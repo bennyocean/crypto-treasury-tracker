c643b531288926639b0fa1a52fc7898ecd825e02
bennyocean <benjamin.schellinger@web.de>
2025-11-02 19:53:36 +0100

    update asset and supply cap list in kpi snapshot upsert writing script

--- scripts/update_kpis_to_sheet.py
+++ scripts/update_kpis_to_sheet.py
@@ -8,18 +8,62 @@ SHEET_NAME = "master_table_v01"
 SNAP_WS     = "kpi_snapshots"
 PRICES_WS   = "prices"
 
-ASSETS = ["BTC", "ETH", "SOL", "XRP", "SUI", "LTC", "HYPE"]
+ASSETS = [
+    "BTC",
+    "ETH",
+    "XRP",
+    "BNB",
+    "SOL",
+    "DOGE",
+    "TRX",
+    "ADA",
+    "SUI",
+    "LTC",
+    "HYPE",
+    "TON",
+    "WLFI",
+    "PUMP",
+    "ATH",
+    "BONK",
+    "AVAX",
+    "CRO",
+    "LINK",
+    "BERA",
+    "TRUMP",
+    "ZIG",
+    "CORE",
+    "VAULTA",
+    "FLUID",
+]
 
 SUPPLY_CAPS = {
-    "BTC": 20_000_000,  
-    "ETH": 120_000_000,
-    "XRP": 60_000_000_000,
-    "BNB": 140_000_000,
-    "SOL": 540_000_000,
-    "SUI": 3_500_000_000,
-    "LTC": 76_000_000,
-    "HYPE": 270_000_000,
-    }
+    "BTC": 19_908_153,
+    "ETH": 120_707_840,
+    "XRP": 59_418_500_720,
+    "BNB": 139_287_622,
+    "SOL": 540_069_892,
+    "DOGE": 150_552_856_383,
+    "TRX": 94_679_730_764,
+    "ADA": 36_448_472_341,
+    "SUI": 3_511_924_479,
+    "LTC": 76_198_926,
+    "HYPE": 270_772_999,
+    "TON": 2_520_529_386,
+    "WLFI": 27_255_958_920,
+    "PUMP": 354_000_000_000,
+    "ATH": 14_234_731_752,
+    "BONK": 77_419_592_329_436,
+    "AVAX": 426_584_745,
+    "CRO": 36_069_453_408,
+    "LINK": 696_849_970,
+    "BERA": 129_478_858,
+    "TRUMP": 199_999_973,
+    "ZIG": 1_408_940_795,
+    "CORE": 1_015_193_271,
+    "VAULTA": 1_599_315_411,
+    "FLUID": 77_753_292,
+}
+
 
 def _get_creds():
     with open("service_account.json", "r") as f:
